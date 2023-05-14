043c027186d7218e679d8e3885b79b7ed9c68328
Dmitrii <godamongmen@bk.ru>
2023-05-14 12:37:58 +0300

    task7

--- Task4.js
+++ Task4.js
@@ -1,3 +1,13 @@
 // Задание 3
 // Напишите функцию repeatWord(word, count), которая принимает слово и количество раз для его повторения. Например, если вызвать repeatWord('слово', 3) функция напечатает "слово1, слово2, слово3" на одной строке.
 
+
+function repeatWord(word, count) {
+    let words = '';
+    for (let i = 1; i <= count; i++) {
+        words = words + word + i + ', ';
+    }
+    console.log(words);
+}
+
+repeatWord('слово', 3)
