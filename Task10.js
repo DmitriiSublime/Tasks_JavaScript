1bbf73570028e03efe2c86960b78e442f856d0c4
Dmitrii <godamongmen@bk.ru>
2023-05-20 12:43:57 +0300

    task10 almost done

--- Task10.js
+++ Task10.js
@@ -2,9 +2,18 @@
 
 m = [7, 3, 21];
 
-document.querySelector('btn').addEventListener('click',() => {
-    for (let i = 0; i < 0; i++) {
-    }
+let numberMap = m.map(item => {
+    console.log(item * 2);
+    return item * 2
 })
+console.log(numberMap);
+
+document.querySelector('.b-1').addEventListener('click', () => {
+    document.querySelector('.out-1').innerHTML = numberMap;
+})
+
+
+
+
 
 
