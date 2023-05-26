cc1dc7d8de8cdc90cc121799b46c7274271f6164
Dmitrii <godamongmen@bk.ru>
2023-05-26 20:23:36 +0300

    task16 not finished

--- Task15.js
+++ Task15.js
@@ -2,18 +2,33 @@
 
 const array = [1, 2, 3, 4, 5];
 
+// let sum = 0;
+// function getNumber() {
+//     for (let i = 0; i < array.length; i++) {
+//         sum += array[i];
+//         return sum
+//     }
+// }
+//
+// getNumber();
+
 function sumNumber() {
     let n = array.reduce((a, b) => a + b);
-    console.log(n);
+    console.log(n([1, 2, 3, 4, 5]));
 }
 
 sumNumber();
 
-function getQuantity() {
-    console.log(array.length);
-}
 
-getQuantity();
 
-let result = sumNumber() / getQuantity();
-console.log(result);
+
+
+// function getQuantity() {
+//     console.log(array.length);
+//     // return array.length;
+// }
+//
+// getQuantity();
+//
+// let result = sumNumber() / getQuantity();
+// console.log(result);
