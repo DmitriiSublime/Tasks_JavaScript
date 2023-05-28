50dff1110e9c820d68789864998ffeaf312dbe77
Dmitrii <godamongmen@bk.ru>
2023-05-28 15:36:33 +0300

    task18 has done

--- Task17.js
+++ Task17.js
@@ -3,16 +3,13 @@
 // Если объект salaries пуст, то результат должен быть 0.
 
 let sum = 0;
-
 let salaries = {
     John: 100,
     Ann: 160,
     Pete: 130
 };
-
 for (let key in salaries) {
     sum += salaries[key];
 }
-
 console.log(sum);
 
