8dcedb55575bff981af5207bd330e81f82e6120e
Dmitrii <115217175+DmitriiSublime@users.noreply.github.com>
2023-06-26 20:27:12 +0300

    Task 22 has done

--- Task22.js
+++ Task22.js
@@ -1,2 +1,9 @@
 // click counter
 
+let counterWidget = document.querySelector('.js-counter');
+counterWidget.addEventListener('click', () => {
+   let counterValueEl = document.querySelector('.js-counter-value');
+   let currentValue = counterValueEl.innerHTML;
+   currentValue++;
+   counterValueEl.innerHTML = currentValue;
+});
